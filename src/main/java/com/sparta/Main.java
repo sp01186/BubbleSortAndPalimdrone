81375f32ac615b9e773080f286b456b90dc81b4f
sp01186 <58943663+sp01186@users.noreply.github.com>
2023-02-07 09:24:19 +0000

    added the bubble sort method

--- src/main/java/com/sparta/Main.java
+++ src/main/java/com/sparta/Main.java
@@ -2,14 +2,20 @@ package com.sparta;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(palimdrome("racecar"));
-        System.out.println(palimdrome("brokenMan"));
-        System.out.println(palimdrome("Dad"));
-        System.out.println(palimdrome("rosiehurtme"));
-        System.out.println(palimdrome("deleveled"));
-        System.out.println(palimdrome("rubbish"));
-        System.out.println(palimdrome("rotor"));
-        System.out.println(palimdrome("madam"));
+//        System.out.println(palimdrome("racecar"));
+//        System.out.println(palimdrome("brokenMan"));
+//        System.out.println(palimdrome("Dad"));
+//        System.out.println(palimdrome("rosiehurtme"));
+//        System.out.println(palimdrome("deleveled"));
+//        System.out.println(palimdrome("rubbish"));
+//        System.out.println(palimdrome("rotor"));
+//        System.out.println(palimdrome("madam"));
+        System.out.println("Hello Genius");
+        int[] array = new int[] {1,3,2,6,0, 4};
+        int[] answerArray = bubbleSort(array);
+        for (int value: answerArray) {
+            System.out.println(value);
+        }
     }
 
     public static boolean palimdrome(String word) {
@@ -28,5 +34,19 @@ public class Main {
             }
         }
         return answer;
+    }
+
+    public static int[] bubbleSort(int[] intArray) {
+        for (int j = intArray.length; j >=0; j--) {
+            for (int i = 0; (i+1) < j; i++) {
+                if (intArray[i] > intArray[i+1]) {
+                    int temp = intArray[i];
+                    intArray[i] = intArray[i+1];
+                    intArray[i+1] = temp;
+                    System.out.println(intArray[i]);
+                }
+            }
+        }
+        return intArray;
     }
 }
